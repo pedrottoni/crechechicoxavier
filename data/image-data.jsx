c8d7300e766e5b81567bb591e263b54f4cc1b952
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2021-09-03 18:32:27 -0300

    Testando o vscode online

--- data/image-data.jsx
+++ data/image-data.jsx
@@ -1,5 +1,5 @@
 export const images = [
-	{ id: 1, src: "/images/img1.jpg", legend: "name", spam: "spam" },
+	{ id: 1, src: "/images/img1.jpg", legend: "Márcia Antunes", spam: "spam" },
 	{ id: 2, src: "/images/img2.jpg", legend: "name", spam: "spam" },
 	{ id: 3, src: "/images/img3.jpg", legend: "name", spam: "spam" },
 	{ id: 4, src: "/images/img4.jpg", legend: "name", spam: "spam" }
