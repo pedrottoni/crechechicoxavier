4ca0624c76587d53ede04d9d37798840c7f1308f
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2021-09-27 16:10:38 -0300

    Testando o Swiper

--- data/transparencia-dados.jsx
+++ data/transparencia-dados.jsx
@@ -1,200 +1,215 @@
 export const transparencia = [
-  {
-    key: 0,
-    link: "fundlegal",
-    title: "Fundamentação",
-    documentos: [
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Estatuto Social"
-      },
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Diretoria Executiva"
-      },
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Termo de Colaboração"
-      },
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Regimento Escolar"
-      },
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Termo Aditivo 01: Termo de colaboração"
-      },
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Proposta Pedagógica: 2019 - 2022"
-      }
-    ]
-  },
-  {
-    key: 1,
-    link: "2021",
-    title: "2021",
-    categorias: [
-      {
-        key: 0,
-        link: "anual",
-        title: "Anual",
-        documentos: [
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          }
-        ]
-      },
-      {
-        key: 1,
-        link: "1trimestre",
-        title: "1º Trimestre",
-        documentos: [
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          }
-        ]
-      },
-      {
-        key: 2,
-        link: "2trimestre",
-        title: "2º Trimestre",
-        documentos: [
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          }
-        ]
-      },
-      {
-        key: 3,
-        link: "3trimestre",
-        title: "3º Trimestre",
-        documentos: [
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          }
-        ]
-      },
-      {
-        key: 4,
-        link: "4trimestre",
-        title: "4º Trimestre",
-        documentos: [
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          }
-        ]
-      },
-      {
-        key: 4,
-        link: "mensal",
-        title: "Mensal",
-        documentos: [
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          },
-          {
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            subtitle: "Documento 1"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    key: 2,
-    link: "anteriores",
-    title: "Anteriores",
-    documentos: [
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Documento 1"
-      },
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Documento 1"
-      },
-      {
-        link:
-          "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-        subtitle: "Documento 1"
-      }
-    ]
-  }
+	{
+		key: 0,
+		link: "fundlegal",
+		title: "Fundamentação",
+		documentos: [
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Estatuto Social",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Diretoria Executiva",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Termo de Colaboração",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Regimento Escolar",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Termo Aditivo 01: Termo de colaboração",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Proposta Pedagógica: 2019 - 2022",
+			},
+		],
+	},
+	{
+		key: 1,
+		link: "2021",
+		title: "2021",
+		categorias: [
+			{
+				key: 0,
+				link: "anual",
+				title: "Anual",
+				documentos: [
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+				],
+			},
+			{
+				key: 1,
+				link: "1trimestre",
+				title: "1º Trimestre",
+				documentos: [
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+				],
+			},
+			{
+				key: 2,
+				link: "2trimestre",
+				title: "2º Trimestre",
+				documentos: [
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+				],
+			},
+			{
+				key: 3,
+				link: "3trimestre",
+				title: "3º Trimestre",
+				documentos: [
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+				],
+			},
+			{
+				key: 4,
+				link: "4trimestre",
+				title: "4º Trimestre",
+				documentos: [
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+				],
+			},
+			{
+				key: 4,
+				link: "mensal",
+				title: "Mensal",
+				documentos: [
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+					{
+						link:
+							"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+						subtitle: "Documento 1",
+					},
+				],
+			},
+		],
+	},
+	{
+		key: 2,
+		link: "anteriores",
+		title: "Anteriores",
+		documentos: [
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Documento 1",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Documento 1",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Documento 1",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Documento 1",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Documento 1",
+			},
+			{
+				link:
+					"https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
+				subtitle: "Documento 1",
+			},
+		],
+	},
 ];
