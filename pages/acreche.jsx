cf1e63bb0ecc3867f5258e6477f3aaccf68886c8
pedrottoni <pedrottoni@gmail.com>
2021-10-21 16:16:23 -0300

    Mobile A Creche

--- pages/acreche.jsx
+++ pages/acreche.jsx
@@ -36,7 +36,7 @@ const useMediaQuery = (width) => {
 export default function Acreche() {
   const isBreakpoint = useMediaQuery(840);
 
-  const [historia, sethistoria] = useState(isBreakpoint ? false : true);
+  const [historia, sethistoria] = useState(isBreakpoint ? true : false);
   const [missao, setMissao] = useState(false);
   const [equipe, setEquipe] = useState(false);
   const [equipeColaboradores, setEquipeColaboradores] = useState(false);
