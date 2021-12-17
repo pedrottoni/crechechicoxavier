976cc720c387b6577d109a297cc9c52f5acc4cc6
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2021-12-17 17:10:19 -0300

    Documentos 2020

--- data/transparencia-dados.jsx
+++ data/transparencia-dados.jsx
@@ -514,13 +514,6 @@ export const transparencia = [
                 span: "RP14",
                 image: "transparência/transparência.svg",
               },
-              /* {
-                key: 4,
-                link: "https://drive.google.com/file/d/1ZbG_vJVUCcqbx5FsQx2flCUyze3vyI8o/view?usp=sharing",
-                title: "Relação de",
-                span: "Tarifas e Reembolso",
-                image: "transparência/transparência.svg",
-              }, */
             ],
           },
           {
@@ -544,7 +537,7 @@ export const transparencia = [
               },
               {
                 key: 2,
-                link: "https://drive.google.com/file/d/10Bk8tXaBwGU4SfFjrdXs30wip29o1ovp/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1WCpvkC1mJmcWF6LGNi5TROGsxUbGwkUj/view?usp=sharing",
                 title: "Demonstrativo",
                 span: "de Despesas",
                 image: "transparência/transparência.svg",
@@ -556,13 +549,6 @@ export const transparencia = [
                 span: "RP14",
                 image: "transparência/transparência.svg",
               },
-              /* {
-                key: 4,
-                link: "https://drive.google.com/file/d/10BkHn0erDTvk9TgXsID5i0vDsaIiGspA/view?usp=sharing",
-                title: "Relação de",
-                span: "Tarifas e Reembolso",
-                image: "transparência/transparência.svg",
-              }, */
             ],
           },
           {
@@ -598,15 +584,6 @@ export const transparencia = [
                 span: "RP14",
                 image: "transparência/transparência.svg",
               },
-              /*
-              {
-                key: 4,
-                link: "https://drive.google.com/file/d/12FlbVYjt_D-r27DiITqOD3uMVSJY4UgN/view?usp=sharing",
-                title: "Relação de",
-                span: "Tarifas e Reembolso",
-                image: "transparência/transparência.svg",
-              },
-              */
             ],
           },
           {
@@ -642,13 +619,6 @@ export const transparencia = [
                 span: "RP14",
                 image: "transparência/transparência.svg",
               },
-              /* {
-                key: 4,
-                link: " ",
-                title: "Relação de",
-                span: "Tarifas e Reembolso",
-                image: "transparência/transparência.svg",
-              }, */
             ],
           },
         ],
@@ -665,28 +635,28 @@ export const transparencia = [
             dados: [
               {
                 key: 0,
-                link: "https://drive.google.com/file/d/19MFS3w9RurlFOlfAFY_Pm0bJqdpq5Iof/view?usp=sharing",
+                link: "https://drive.google.com/file/d/143ILlTjZnYVc0Ahxjk9HHpgatoEBI2BG/view?usp=sharing",
                 title: "Plano de",
                 span: "Gestão",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 1,
-                link: "https://drive.google.com/file/d/1unffgXxO0MEeIh9Dd1ev8I_4LK0zd6HL/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1Rytd-sL0uZ32DF-McTX0H4werRUwygbx/view?usp=sharing",
                 title: "Plano de",
                 span: "Trabalho",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 2,
-                link: "https://drive.google.com/file/d/1eFSlgp4B-QuxUxAX_hBAaUmTk0LMYOeI/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1lAtyxWgYImgKORaf4bkM8nx6yIdecEHI/view?usp=sharing",
                 title: "Atestado de",
                 span: "Funcionamento",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 3,
-                link: "https://drive.google.com/file/d/1BScjsSVhG6QtVmyosWbJybeUGiX6VgM7/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1OST_JFs3hviBjjjncejap9vkJvzaf_s-/view?usp=sharing",
                 title: "Parecer da ",
                 span: "assessoria EI SME",
                 image: "transparência/transparência.svg",
@@ -700,79 +670,76 @@ export const transparencia = [
             dados: [
               {
                 key: 0,
-                link: "https://drive.google.com/file/d/1GZgX20l0zRtpPJe1yjR31aJ2-zY2Fs1b/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1Aq_5AjZO_Lcq9XqwAaPjOwu99htdfuT5/view?usp=sharing",
                 title: "Janeiro",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 1,
-                link: "https://drive.google.com/file/d/1qsQtvfhqVVo_c79R0AxWuFJSELfAp_Yx/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1t9sZZkg21ZhtHSMQYUzwvKaWxA_iiI2t/view?usp=sharing",
                 title: "Fevereiro",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 2,
-                link: "https://drive.google.com/file/d/1ggd2Ds-1ZD9GapNIP6bysk7EFbScmykt/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1Sm4XKJWnbSDZ-3JLTCXoNvtJirD1NaOC/view?usp=sharing",
                 title: "Março",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 3,
-                link: "https://drive.google.com/file/d/10CBfTqcmJKm363sBA7jjcaEWtT3CLfEk/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1COrA7D1E3BJGN8zzvT03FoZMpmiUiGZw/view?usp=sharing",
                 title: "Abril",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 4,
-                link: "https://drive.google.com/file/d/10CnCay_3QNZq1HaWVQ2ml0fSyMSbFTkJ/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1IpddCx345zTwYC2cFPrE3mwMbtuK-9nC/view?usp=sharing",
                 title: "Maio",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 5,
-                link: "https://drive.google.com/file/d/10DK3Oqn8hEElFB3xQBB0VKdCpHycV7Pl/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1iWbIWvdikO3JdoCsav_4AWnZ7atoyg_u/view?usp=sharing",
                 title: "Junho",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 6,
-                link: "https://drive.google.com/file/d/12LBdrcCzYMFdDG01L13gNUAE8PIvMha8/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1r475Upj2CET2P9bilET2Kdn7DmLgX5EB/view?usp=sharing",
                 title: "Julho",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 7,
-                link: "https://drive.google.com/file/d/12OvxivWqUINUk2RgH19_Hr08CsIRNOA4/view?usp=sharing",
+                link: "https://drive.google.com/file/d/11UtPrHqXHQaEWl_suVXxriIGJ7gREI5J/view?usp=sharing",
                 title: "Agosto",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 8,
-                link: "https://drive.google.com/file/d/12Qbb11RLu2-SxQcbvWDNDy56qwicH1_N/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1HSOdidYQRPsYW6l9iDVCByBxAkKjj6iX/view?usp=sharing",
                 title: "Setembro",
                 image: "transparência/transparência.svg",
               },
-              /*{
-            key: 9,
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            title: "Outubro",
-            image: "transparência/transparência.svg"
-          },
-          {
-            key: 10,
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            title: "Novembro",
-            image: "transparência/transparência.svg"
-          },
-          {
-            key: 11,
-            link:
-              "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view",
-            title: "Dezembro",
-            image: "transparência/transparência.svg"
-          } */
+              {
+                key: 9,
+                link: "https://drive.google.com/file/d/1Qu2ffTNulAnXZkuJlMLf6FezASjlOZcZ/view?usp=sharing",
+                title: "Outubro",
+                image: "transparência/transparência.svg",
+              },
+              {
+                key: 10,
+                link: "https://drive.google.com/file/d/1pEFnp1GucS0F5AOmuInl03hSotMvenQ_/view?usp=sharing",
+                title: "Novembro",
+                image: "transparência/transparência.svg",
+              },
+              {
+                key: 11,
+                link: "https://drive.google.com/file/d/1PItig1m_0vYrtzpGelMdRgr6A0jo6rmc/view?usp=sharing",
+                title: "Dezembro",
+                image: "transparência/transparência.svg",
+              },
             ],
           },
           {
@@ -782,35 +749,35 @@ export const transparencia = [
             dados: [
               {
                 key: 0,
-                link: "https://drive.google.com/file/d/1j2UBqEJbdYIoJyj5QsTagg1Ipc22N9Aj/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1f7JfYXQpxsYJFzFKtNDiuswyVcFy3Sn6/view?usp=sharing",
                 title: "Relatório",
                 span: "Técnico",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 1,
-                link: "https://drive.google.com/file/d/1_PYofRSZQTi6y1wprzIVOn8O_lFKAZjw/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1kcdn_bTJ-6y8M6Sfs9OCewFoGNcV1YMP/view?usp=sharing",
                 title: "Relatório",
                 span: "Pedagógico",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 2,
-                link: "https://drive.google.com/file/d/1Wy5lGdUjtqaPZP0XqYvz0XvR7enuTGP4/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1Gd9msr28pm2LN63xPbP7BxHBAVzRIU6w/view?usp=sharing",
                 title: "Demonstrativo",
                 span: "de Despesas",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 3,
-                link: "https://drive.google.com/file/d/1EkEyE2q0-BdBNXtqBNTBjM00yC8Q_J0Q/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1ad8vEsKnUPrOLEXUlEVbSM7bgRGuy-9N/view?usp=sharing",
                 title: "Anexo",
                 span: "RP14",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 4,
-                link: "https://drive.google.com/file/d/1ZbG_vJVUCcqbx5FsQx2flCUyze3vyI8o/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1rdiWH3ebC-Jxr2ZbcEO7D8rmB-AN-l2w/view?usp=sharing",
                 title: "Relação de",
                 span: "Tarifas e Reembolso",
                 image: "transparência/transparência.svg",
@@ -824,35 +791,28 @@ export const transparencia = [
             dados: [
               {
                 key: 0,
-                link: "https://drive.google.com/file/d/10DP5Ymh-LS6yS-aUxG1V7dQyR39ysXIg/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1O4WII8ZeuCgpBYe7knWafmDNz1-KvxbT/view?usp=sharing",
                 title: "Relatório",
                 span: "Técnico",
                 image: "transparência/transparência.svg",
               },
               {
                 key: 1,
-                link: "https://drive.google.com/file/d/1_PYofRSZQTi6y1wprzIVOn8O_lFKAZjw/view?usp=sharing",
-                title: "Relatório",
-                span: "Pedagógico",
-                image: "transparência/transparência.svg",
-              },
-              {
-                key: 2,
-                link: "https://drive.google.com/file/d/10Bk8tXaBwGU4SfFjrdXs30wip29o1ovp/view?usp=sharing",
+                link: "https://drive.google.com/file/d/11tUBL_-7lbNrgfuDzcIRnyWltp_gA3Uc/view?usp=sharing",
                 title: "Demonstrativo",
                 span: "de Despesas",
                 image: "transparência/transparência.svg",
               },
               {
-                key: 3,
-                link: "https://drive.google.com/file/d/10BcjeMebewZ1pBRO4R-b9GrmqIz-nLVu/view?usp=sharing",
+                key: 2,
+                link: "https://drive.google.com/file/d/1uzfv4qW0O6eyMVYvd1R08ExBbCUzwskZ/view?usp=sharing",
                 title: "Anexo",
                 span: "RP14",
                 image: "transparência/transparência.svg",
               },
               {
-                key: 4,
-                link: "https://drive.google.com/file/d/10BkHn0erDTvk9TgXsID5i0vDsaIiGspA/view?usp=sharing",
+                key: 3,
+                link: "https://drive.google.com/file/d/1CeWMwphRTdg-Hh2oyTvqNUKjTQj_Ygx2/view?usp=sharing",
                 title: "Relação de",
                 span: "Tarifas e Reembolso",
                 image: "transparência/transparência.svg",
@@ -866,97 +826,69 @@ export const transparencia = [
             dados: [
               {
                 key: 0,
-                link: "https://drive.google.com/file/d/12eI_3cIizJGqzy6uvqUE4ojKLTrN6WBZ/view?usp=sharing",
+                link: "https://drive.google.com/file/d/1mK0xXCJzAysWie5l3Hne1c-OJQKANqeu/view?usp=sharing",
                 title: "Relatório",
                 span: "Técnico",
                 image: "transparência/transparência.svg",
               },
-              /*
-          {
-            key: 1,
-            link:
-              " ",
-            title: "Relatório",
-            span: "Pedagógico",
-            image: "transparência/transparência.svg"
-          },*/
               {
-                key: 2,
-                link: "https://drive.google.com/file/d/12DXT5-95Bbjy1oxZ5uTmxlwsjg9acB5q/view?usp=sharing",
+                key: 1,
+                link: "https://drive.google.com/file/d/1SUC2tN88wBtQAHls9cRvZ1yRe0eCpGxW/view?usp=sharing",
                 title: "Demonstrativo",
                 span: "de Despesas",
                 image: "transparência/transparência.svg",
               },
               {
-                key: 3,
-                link: "https://drive.google.com/file/d/12CfN_bBKtWLyMIK9rfTMVJj06UNw4gsx/view?usp=sharing",
+                key: 2,
+                link: "https://drive.google.com/file/d/10gJK-rl3abnQvx9T9fUnHMNZLHb3Ydhf/view?usp=sharing",
                 title: "Anexo",
                 span: "RP14",
                 image: "transparência/transparência.svg",
               },
               {
-                key: 4,
-                link: "https://drive.google.com/file/d/12FlbVYjt_D-r27DiITqOD3uMVSJY4UgN/view?usp=sharing",
+                key: 3,
+                link: "https://drive.google.com/file/d/1uunDj6DpGR-S_-M3YhfoG8og0-k5GT6y/view?usp=sharing",
                 title: "Relação de",
                 span: "Tarifas e Reembolso",
                 image: "transparência/transparência.svg",
               },
+            ],
+          },
+          {
+            key: 5,
+            link: "4trimestre",
+            span: "4º Trimestre",
+            dados: [
               {
-                key: 5,
-                link: "https://drive.google.com/file/d/12XOsBroSeOcT8h2ZhGaf5lPb5XFTAIx4/view?usp=sharing",
-                title: "Justificativa",
-                span: "Trimestral",
+                key: 0,
+                link: "https://drive.google.com/file/d/1r3gPXM8kZkOpokCN-8MtA4P3vTAmp3xT/view?usp=sharing",
+                title: "Relatório",
+                span: "Técnico",
+                image: "transparência/transparência.svg",
+              },
+              {
+                key: 1,
+                link: "https://drive.google.com/file/d/15rl3VpKED_H_J-bwXp2VB_ItlMDDKRNs/view?usp=sharing",
+                title: "Demonstrativo",
+                span: "de Despesas",
+                image: "transparência/transparência.svg",
+              },
+              {
+                key: 2,
+                link: "https://drive.google.com/file/d/1sE4oOpGQruuoyt5Qmi1MXFvNjXy4PFgy/view?usp=sharing",
+                title: "Anexo",
+                span: "RP14",
+                image: "transparência/transparência.svg",
+              },
+              {
+                key: 3,
+                link: "https://drive.google.com/file/d/1GBAZPQGB0K1r94PFlCsPfhF_RQxl0PEy/view?usp=sharing",
+                title: "Relação de",
+                span: "Tarifas e Reembolso",
                 image: "transparência/transparência.svg",
               },
             ],
           },
-          /* {
-        key: 5,
-        link: "4trimestre",
-        span: "4º Trimestre",
-        dados: [
-          {
-            key: 0,
-            link:
-              " ",
-            title: "Relatório",
-            span: "Técnico",
-            image: "transparência/transparência.svg"
-          },
-          {
-            key: 1,
-            link:
-              " ",
-            title: "Relatório",
-            span: "Pedagógico",
-            image: "transparência/transparência.svg"
-          },
-          {
-            key: 2,
-            link:
-              " ",
-            title: "Demonstrativo",
-            span: "de Despesas",
-            image: "transparência/transparência.svg"
-          },
-          {
-            key: 3,
-            link:
-              " ",
-            title: "Anexo",
-            span: "RP14",
-            image: "transparência/transparência.svg"
-          },
-          {
-            key: 4,
-            link:
-              " ",
-            title: "Relação de",
-            span: "Tarifas e Reembolso",
-            image: "transparência/transparência.svg"
-          }
-        ]
-      }, */
         ],
       },
     ],
